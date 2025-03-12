6af731cc157d0e0ffba50eca63e7754667f7b75c
Antoni Metge <mtn5047a@ensfsi.onmicrosoft.com>
2025-03-12 11:54:40 +0100

    debut tp23

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -1,6 +1,7 @@
 package frontiere;
 
 import controleur.ControlPrendreEtal;
+import frontiere.Clavier;
 
 public class BoundaryPrendreEtal {
 	private ControlPrendreEtal controlPrendreEtal;
@@ -10,10 +11,24 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		if (!controlPrendreEtal.verifierIdentite(nomVendeur)) {
+			System.out.println("Je suis desolee " + nomVendeur + " mais il faut etre un habitant du village pour commercer ici.");
+		} else {
+			System.out.println("Bonjour " + nomVendeur + ", je vais regarder si je peux vous trouver un etal.");
+			if (!controlPrendreEtal.resteEtals()) {
+				System.out.println("Desolee "+ nomVendeur + ", je n'ai plus d'etal qui ne sois pas deja occupe.");
+			} else {
+				installerVendeur(nomVendeur);
+			}
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
-		//TODO a completer
+		StringBuilder question = new StringBuilder();
+		question.append("c'est parfait il me reste un etal pour vous !\n");
+		question.append("il me faudrait quelques renseignement :\n");
+		Clavier.entrerChaine(question.toString);
+		
+		
 	}
 }
